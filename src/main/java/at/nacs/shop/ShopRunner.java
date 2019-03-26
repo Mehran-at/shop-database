11f6da610a3bb505bed3044b5fa88926290050b0
Mehran Hosseini <ajaxcube@gmail.com>
2019-03-26 15:18:38 -0400

    inital

---
+++ src/main/java/at/nacs/shop/ShopRunner.java
@@ -0,0 +1,26 @@
+package at.nacs.shop;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ShopRunner {
+    @Bean
+    ApplicationRunner runner(List<Item>items, ItemRepository repository) {
+        return args -> {
+            System.out.println("---DELETE---");
+            repository.deleteAll();
+
+            System.out.println("---Save---");
+            repository.saveAll(items);
+
+            System.out.println("---FIND---");
+            List<Item> all = repository.findAll();
+            all.forEach(System.out::println);
+        };
+    }
+}
